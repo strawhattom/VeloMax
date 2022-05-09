e1c4b6b267dac75e3d945805c69b4cfd9c5fc9d0
Iman Alexandre TOURRES <contact@nami10.fr>
2022-05-09 13:52:04 +0200

    avalonia mvvm template

---
+++ VeloMax/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VeloMax.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
