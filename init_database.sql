95885dd05fec80fb15919c0b17a0fa475c05da64
sir <66173239+strawhattom@users.noreply.github.com>
2022-05-16 15:41:06 +0200

    remove auto increment in professional table

--- init_database.sql
+++ init_database.sql
@@ -8,14 +8,14 @@ USE velomax;
 -- Clients
 DROP TABLE IF EXISTS clients;
 CREATE TABLE IF NOT EXISTS clients(
-    id INT NOT NULL AUTO_INCREMENT,
-    street VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    postal_code VARCHAR(255) NOT NULL,
-    province VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
-    mail VARCHAR(255) NOT NULL,
-    PRIMARY KEY(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	street VARCHAR(255) NOT NULL,
+	city VARCHAR(255) NOT NULL,
+	postal_code VARCHAR(255) NOT NULL,
+	province VARCHAR(255) NOT NULL,
+	phone VARCHAR(255) NOT NULL,
+	mail VARCHAR(255) NOT NULL,
+	PRIMARY KEY(id)
 );
 
 DROP TABLE IF EXISTS individuals;
@@ -27,17 +27,17 @@ CREATE TABLE IF NOT EXISTS individuals(
 	PRIMARY KEY(id),
 	FOREIGN KEY(id)
 		REFERENCES clients(id),
-    FOREIGN KEY(id_fidelity)
-        REFERENCES fidelity_programs(id)
+    	FOREIGN KEY(id_fidelity)
+        	REFERENCES fidelity_programs(id)
 );
 
 DROP TABLE IF EXISTS professionals;
 CREATE TABLE IF NOT EXISTS professionals(
-    id INT NOT NULL AUTO_INCREMENT,
-    company_name VARCHAR(255) NOT NULL,
-    contact_name VARCHAR(255) NOT NULL,
-    order_count INT NOT NULL DEFAULT 0,
-    PRIMARY KEY(id),
+	id INT NOT NULL,
+	company_name VARCHAR(255) NOT NULL,
+	contact_name VARCHAR(255) NOT NULL,
+	order_count INT NOT NULL DEFAULT 0,
+	PRIMARY KEY(id),
 	FOREIGN KEY(id)
 		REFERENCES clients(id)
 );
@@ -46,108 +46,108 @@ CREATE TABLE IF NOT EXISTS professionals(
 -- Orders
 DROP TABLE IF EXISTS orders;
 CREATE TABLE IF NOT EXISTS orders(
-    id INT NOT NULL AUTO_INCREMENT,
-    order_date DATE NOT NULL,
-    shipping_address VARCHAR(255) NOT NULL,
-    shipping_date DATE NOT NULL,
-    quantity INT NOT NULL,
-    PRIMARY KEY(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	order_date DATE NOT NULL,
+	shipping_address VARCHAR(255) NOT NULL,
+	shipping_date DATE NOT NULL,
+	quantity INT NOT NULL,
+	PRIMARY KEY(id)
 );
 
 -- Bikes
 DROP TABLE IF EXISTS bikes;
 CREATE TABLE IF NOT EXISTS bikes(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    target VARCHAR(255) NOT NULL,
-    unit_price FLOAT NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    introduction_date DATE NOT NULL,
-    discontinuation_date DATE NOT NULL,
-    PRIMARY KEY(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	target VARCHAR(255) NOT NULL,
+	unit_price FLOAT NOT NULL,
+	type VARCHAR(255) NOT NULL,
+	introduction_date DATE NOT NULL,
+	discontinuation_date DATE NOT NULL,
+	PRIMARY KEY(id)
 );
 
 -- Parts
 DROP TABLE IF EXISTS parts;
 CREATE TABLE IF NOT EXISTS parts(
-    id INT NOT NULL AUTO_INCREMENT,
-    description VARCHAR(255) NOT NULL,
-    unit_price FLOAT NULL,
-    introduction_date DATE NOT NULL,
-    discontinuation_date DATE NOT NULL,
-    procurement_delay INT DEFAULT 0,
-    quantity INT NOT NULL DEFAULT 1,
-    type VARCHAR(255) NOT NULL,
-    PRIMARY KEY(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	description VARCHAR(255) NOT NULL,
+	unit_price FLOAT NULL,
+	introduction_date DATE NOT NULL,
+	discontinuation_date DATE NOT NULL,
+	procurement_delay INT DEFAULT 0,
+	quantity INT NOT NULL DEFAULT 1,
+	type VARCHAR(255) NOT NULL,
+	PRIMARY KEY(id)
 );
 
 -- Suppliers
 DROP TABLE IF EXISTS suppliers;
 CREATE TABLE IF NOT EXISTS suppliers(
-    id INT NOT NULL AUTO_INCREMENT,
-    siret VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    contact VARCHAR(255) NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    label ENUM('1','2','3','4'), -- 1 very good, 2 good, 3 average, 4 bad
-    PRIMARY KEY(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	siret VARCHAR(255) NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	contact VARCHAR(255) NOT NULL,
+	location VARCHAR(255) NOT NULL,
+	label ENUM('1','2','3','4'), -- 1 very good, 2 good, 3 average, 4 bad
+	PRIMARY KEY(id)
 );
 
 -- Fidelity programs
 DROP TABLE IF EXISTS fidelity_programs;
 CREATE TABLE IF NOT EXISTS fidelity_programs(
-    id INT NOT NULL AUTO_INCREMENT,
-    label VARCHAR(255) NOT NULL,
-    cost INT NOT NULL,
-    duration INT NOT NULL,
-    discount INT NOT NULL CHECK(discount < 100), -- percentage
-    PRIMARY KEY(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	label VARCHAR(255) NOT NULL,
+	cost INT NOT NULL,
+	duration INT NOT NULL,
+	discount INT NOT NULL CHECK(discount < 100), -- percentage
+	PRIMARY KEY(id)
 );
 
 -- Ordered bikes
 DROP TABLE IF EXISTS ordered_bikes;
 CREATE TABLE IF NOT EXISTS oredered_bikes(
-    id INT NOT NULL AUTO_INCREMENT,
-    orders_id INT NOT NULL,
-    bikes_id INT NOT NULL,
-    quantity INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(orders_id) REFERENCES orders(id),
-    FOREIGN KEY(bikes_id) REFERENCES bikes(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	orders_id INT NOT NULL,
+	bikes_id INT NOT NULL,
+	quantity INT NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(orders_id) REFERENCES orders(id),
+	FOREIGN KEY(bikes_id) REFERENCES bikes(id)
 );
 
 -- Ordered parts
 DROP TABLE IF EXISTS ordered_parts;
 CREATE TABLE IF NOT EXISTS oredered_parts(
-    id INT NOT NULL AUTO_INCREMENT,
-    orders_id INT NOT NULL,
-    parts_id INT NOT NULL,
-    quantity INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(orders_id) REFERENCES orders(id),
-    FOREIGN KEY(parts_id) REFERENCES parts(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	orders_id INT NOT NULL,
+	parts_id INT NOT NULL,
+	quantity INT NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(orders_id) REFERENCES orders(id),
+	FOREIGN KEY(parts_id) REFERENCES parts(id)
 );
 
 -- Bike parts
 DROP TABLE IF EXISTS bike_parts;
 CREATE TABLE IF NOT EXISTS bike_parts(
-    id INT NOT NULL AUTO_INCREMENT,
-    parts_id INT NOT NULL,
-    bikes_id INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(parts_id) REFERENCES parts(id),
-    FOREIGN KEY(bikes_id) REFERENCES bikes(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	parts_id INT NOT NULL,
+	bikes_id INT NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(parts_id) REFERENCES parts(id),
+	FOREIGN KEY(bikes_id) REFERENCES bikes(id)
 );
 
 -- Procurement
 DROP TABLE IF EXISTS procurement;
 CREATE TABLE IF NOT EXISTS procurement(
-    id INT NOT NULL AUTO_INCREMENT,
-    parts_id INT NOT NULL,
-    suppliers_id INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(parts_id) REFERENCES parts(id),
-    FOREIGN KEY(suppliers_id) REFERENCES suppliers(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	parts_id INT NOT NULL,
+	suppliers_id INT NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(parts_id) REFERENCES parts(id),
+	FOREIGN KEY(suppliers_id) REFERENCES suppliers(id)
 );
 
 -- EOF
