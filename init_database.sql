52b1ede23d5b599e79657c97ca803572178b6315
Vct_rx <vic.tarroux@gmail.com~>
2022-05-16 14:55:41 +0200

    ajout heritage clients

--- init_database.sql
+++ init_database.sql
@@ -9,21 +9,35 @@ USE velomax;
 DROP TABLE IF EXISTS clients;
 CREATE TABLE IF NOT EXISTS clients(
     id INT NOT NULL AUTO_INCREMENT,
-    type ENUM("profesionnal", "individual"),
-    company_name VARCHAR(255) NULL,
-    last_name VARCHAR(255) NULL,
-    first_name VARCHAR(255) NULL,
     street VARCHAR(255) NOT NULL,
     city VARCHAR(255) NOT NULL,
     postal_code VARCHAR(255) NOT NULL,
     province VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL,
     mail VARCHAR(255) NOT NULL,
-    fidelity_program INT DEFAULT 0,
-    member BOOLEAN NOT NULL DEFAULT 0,
-    order_count INT NULL DEFAULT 0,
     PRIMARY KEY(id)
 );
+
+DROP TABLE IF EXISTS Particulier;
+CREATE TABLE IF NOT EXISTS Particulier(
+	id INT NOT NULL,
+	first_name VARCHAR(255) NULL,
+	last_name VARCHAR(255) NULL,
+	id_fidelity_programs INT DEFAULT 0,
+	primary key(id),
+	foreign key(id)
+		references clients(id)
+);
+
+DROP TABLE IF EXISTS Professionel;
+CREATE TABLE IF NOT EXISTS Professionel(
+    id INT NOT NULL AUTO_INCREMENT,
+	company_name VARCHAR(255) NULL,
+	order_count INT NULL DEFAULT 0,
+    primary key(id),
+    foreign key(id)
+		references clients(id));
+
 
 -- Orders
 DROP TABLE IF EXISTS orders;
