fa8408403489a120dfe7b6a82d31518ef7d38a9c
Tom Xie <xie.tom7@gmail.com>
2022-05-18 12:04:44 +0200

    part edit window

---
+++ VeloMax/Views/PartUpdateWindow.axaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace VeloMax.Views
+{
+    public partial class PartUpdateWindow: Window
+    {
+        public PartUpdateWindow()
+        {
+
+            InitializeComponent();
+            DatePicker IntroductionDP = new DatePicker()
+            {
+                Header = "Introduction date"
+            };
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
