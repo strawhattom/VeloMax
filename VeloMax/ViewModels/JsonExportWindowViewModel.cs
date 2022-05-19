597a609d1b31d1b6029b9ac027c3819d0c2f7cbf
Iman Alexandre TOURRES <contact@nami10.fr>
2022-05-19 10:32:55 +0200

    Json Export Fix

--- VeloMax/ViewModels/JsonExportWindowViewModel.cs
+++ VeloMax/ViewModels/JsonExportWindowViewModel.cs
@@ -7,6 +7,7 @@ using VeloMax.Services;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Xml;
+using Avalonia.Controls;
 
 namespace VeloMax.ViewModels;
 
@@ -15,20 +16,22 @@ public class JsonExportWindowViewModel : ViewModelBase
     public ReactiveCommand<Unit, Unit> ExportJson { get; set; }
     
     private int _minQty;
-    private string _filePath = "Salut";
+    private string _filePath = "parts.json";
+    private string _data = "";
+    private string _color = "black";
     
     private readonly Database _db = new();
     public JsonExportWindowViewModel()
     {
         ExportJson = ReactiveCommand.Create(() =>
         {
-            JsonPart Parts = new JsonPart(new ObservableCollection<object>(_db.MinQtyParts(_minQty)));
+            ObservableCollection<object> Parts = new ObservableCollection<object>(_db.MinQtyParts(_minQty));
             var options = new JsonSerializerOptions { WriteIndented = true };
             string jsonString = JsonSerializer.Serialize(Parts, options);
             //Console.WriteLine(Parts[0]);
             //Console.WriteLine(jsonString);
             File.WriteAllText(_filePath, jsonString);
-            Console.WriteLine("OK");
+            Console.WriteLine("OK"); 
         });
     }
 
@@ -43,14 +46,4 @@ public class JsonExportWindowViewModel : ViewModelBase
         get => _filePath;
         set => this.RaiseAndSetIfChanged(ref _filePath, value);
     }
-}
-
-public class JsonPart
-{
-    public ObservableCollection<object> Parts;
-
-    public JsonPart(ObservableCollection<object> Parts)
-    {
-        Parts = Parts;
-    } 
 }
