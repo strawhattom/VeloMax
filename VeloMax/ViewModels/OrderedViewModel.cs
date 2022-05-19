99e478146642a6cbcb993d653eaf997801400bfd
Tom Xie <xie.tom7@gmail.com>
2022-05-19 11:00:42 +0200

    ordered view working

--- VeloMax/ViewModels/OrderedViewModel.cs
+++ VeloMax/ViewModels/OrderedViewModel.cs
@@ -1,15 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using ReactiveUI;
 using VeloMax.Models;
 
 namespace VeloMax.ViewModels
 {
     public class OrderedViewModel : ViewModelBase
     {
+        
+        private OrderedPart _selectPart;
+        private OrderedBike _selectBike;
         public ObservableCollection<OrderedBike> OrderedBikes { get; } // don't change it
         public ObservableCollection<OrderedPart> OrderedParts { get; } // don't change it
 
+        public OrderedPart SelectedPart
+        {
+            get => _selectPart;
+            set => this.RaiseAndSetIfChanged(ref _selectPart, value);
+        }
+        
+        public OrderedBike SelectedBike
+        {
+            get => _selectBike;
+            set => this.RaiseAndSetIfChanged(ref _selectBike, value);
+        }
         public OrderedViewModel(List<OrderedBike> ob, List<OrderedPart> op)
         {
             OrderedBikes = new ObservableCollection<OrderedBike>(ob);
