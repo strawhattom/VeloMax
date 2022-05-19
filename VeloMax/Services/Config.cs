5e22c782b6d8a7eedeb9920ee4c7ca6a05d0c894
Tom Xie <xie.tom7@gmail.com>
2022-05-19 10:50:32 +0200

    add config file

---
+++ VeloMax/Services/Config.cs
@@ -0,0 +1,8 @@
+﻿/* in ./Services/Config.cs */
+namespace VeloMax.Services
+{
+    public static class Config
+    {
+        public static string Connection => "database=velomax;server=localhost;uid=root;pwd=root";
+    }
+}
